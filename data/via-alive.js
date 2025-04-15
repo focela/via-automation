b5cfab5bb8b54f29318c6b051a2887a2d42859a6
Son Tran Thanh <286.trants@gmail.com>
2025-04-16 02:24:40 +0700

    2

--- data/via-alive.js
+++ data/via-alive.js
@@ -2,6 +2,12 @@ export default [
   {
     "id": "via-61561470050187",
     "sessionPath": "sessions/via-61561470050187.json",
+    "proxy": {
+      "type": "residential",
+      "server": "http://us.smartproxy.com:10001",
+      "username": "spxuasi28c",
+      "password": "wxh29u5R2Ixevu=WnB"
+    },
     "userAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)..."
   }
 ];
